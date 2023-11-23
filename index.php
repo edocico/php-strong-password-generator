1d48e677bef56708486365308b274257abdf08b3
edocico <138378767+edocico@users.noreply.github.com>
2023-11-23 15:52:51 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+$number = $_GET['number'] ?? null;
+
+$letters = "abcdefghijklmnopqrstxywuvz";
+
+var_dump($letters)
+
+function getrandompassword($chosenNum, $lettersPull) {
+
+    $password = [];
+    while
+}
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>PHP Strong Password Generator</title>
+</head>
+<body>
+
+    <div class="title">
+        <h1>PHP Strong Password Generator</h1>
+    </div>
+    <div class="form-input">
+        <p>Inserisci il numero di lettere da cui vuoi che sia composta la tua password</p>
+        <form class="form" action="" method="GET">
+            <input class="input" type="number" placeholder="inserisci un numero" name="number">
+            <input class="button" type="submit" value="invia">
+        </form> 
+    </div>
+    
+</body>
+</html>
